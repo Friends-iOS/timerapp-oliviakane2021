8a9d06927ed99b9dcb33a963dbcb1813e1351a1b
Olivia <21kane@friendsseminary.org>
2020-04-04 18:28:11 -0400

    timer hw project

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let toastTimes = ["light": 5, "golden": 8, "burnt": 11]
+    
+    var timer = Timer()
+    
+    let toastSelector: String = "light"
+    
+    var totalTime: Float = 0
+   
+    var secondsPassed: Float = 0
+    
+    var player: AVAudioPlayer!
+    
+    var toast: String = ""
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBAction func toastSelected(_ sender: UIButton) {
+        
+        self.titleLabel.text = "How Do You Like Your Toast?"
+        
+        timer.invalidate()
+        
+        secondsPassed = 0
+        
+        totalTime = 0
+        
+        toast = sender.currentTitle!
+        print(toast)
+        
+        totalTime = Float(toastTimes[toast]!)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.timerAction), userInfo: nil, repeats: true)
+
+    
     }
+    
+    @objc func timerAction() {
+        
+        if (totalTime > 0){
+            self.titleLabel.text = String(totalTime)
+            totalTime = totalTime-1
+        }
+        
+        else{
+            self.titleLabel.text = "DONE!"
+            playSound(label: "alarm_sound")
+            timer.invalidate()
+        }
+    }
+   
+   
+    func playSound(label : String) {
+        let url = Bundle.main.url(forResource: label, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+    //}
 
 
+    
 }
 
